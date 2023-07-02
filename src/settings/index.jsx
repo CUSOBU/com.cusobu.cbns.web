6d13f9ce92435ccac15bd58fc8d9095f477c7299
Geider <garevaloqq@gmail.com>
2023-07-01 23:04:49 -0300

    dfddd

--- src/settings/index.jsx
+++ src/settings/index.jsx
@@ -1,37 +1,36 @@
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import SmsFailedIcon from '@mui/icons-material/SmsFailed';
-import AutorenewIcon from '@mui/icons-material/Autorenew';
-import DoneAllIcon from '@mui/icons-material/DoneAll';
+import SmsFailedIcon from "@mui/icons-material/SmsFailed";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
 import env from "../utils/env";
-
 
 export const navigation = [
   {
     title: "Dashboard",
     exact: false,
-    link: `${env.url}/`,
-    icon: <DashboardIcon/>,
+    link: "/",
+    icon: <DashboardIcon />,
   },
   {
     header: true,
-    title: `Remittances`,
+    title: "Remittances",
   },
   {
     title: "Procesando",
     exact: false,
-    link: `/remittances`,
-    icon: <AutorenewIcon/>,
+    link: "/remittances",
+    icon: <AutorenewIcon />,
   },
   {
     title: "Completadas",
     exact: false,
-    link: `/remittances_completed`,
+    link: "/remittances_completed",
     icon: <DoneAllIcon />,
   },
   {
     title: "Fallidas",
     exact: false,
-    link: `/remittances_fails`,
+    link: "/remittances_fails",
     icon: <SmsFailedIcon />,
   },
 ];
