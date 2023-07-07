d4ef542dcd6dd328cd74e0a2170e5f6a20ab05df
Geider <garevaloqq@gmail.com>
2023-07-07 19:51:57 -0300

    fisrt commit

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,12 +1,12 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 // assets
-import colors from '../assets/scss/_themes-vars.module.scss';
+import colors from "../assets/scss/_themes-vars.module.scss";
 
 // project imports
-import componentStyleOverrides from './compStyleOverride';
-import themePalette from './palette';
-import themeTypography from './typography';
+import componentStyleOverrides from "./compStyleOverride";
+import themePalette from "./palette";
+import themeTypography from "./typography";
 
 /**
  * Represent theme style and structure as per Material-UI
@@ -28,12 +28,27 @@ export const theme = (customization) => {
     menuSelected: color.secondaryDark,
     menuSelectedBack: color.secondaryLight,
     divider: color.grey200,
-    customization
+    customization,
   };
 
   const themeOptions = {
+    direction: "ltr",
     palette: themePalette(themeOption),
-    typography: themeTypography(themeOption)
+    mixins: {
+      toolbar: {
+        minHeight: "38px",
+        padding: "4px",
+        "@media (min-width: 600px)": {
+          minHeight: "38px",
+        },
+      },
+      content: {
+        flexGrow: 1,
+        height: "100vh",
+        overflow: "auto",
+      },
+    },
+    typography: themeTypography(themeOption),
   };
 
   const themes = createTheme(themeOptions);
