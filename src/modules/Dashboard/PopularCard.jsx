92226e77821b93bae647b5c9a9821defef83b6ef
Geider <garevaloqq@gmail.com>
2023-07-08 18:45:27 -0300

    Refactory

--- src/modules/Dashboard/PopularCard.jsx
+++ src/modules/Dashboard/PopularCard.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
 
 // material-ui
 import { useTheme } from "@mui/material/styles";
@@ -10,38 +9,22 @@ import {
   CardContent,
   Divider,
   Grid,
-  Menu,
-  MenuItem,
   Typography,
 } from "@mui/material";
 
 // project imports
-import BajajAreaChartCard from "./BajajAreaChartCard";
 import OperationsAreaChartCard from "./OperationsAreaChartCard";
 import MainCard from "../../components/cards/MainCard";
 import SkeletonPopularCard from "../../components/cards/Skeleton/PopularCard";
 
 // assets
 import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined";
-import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined";
-import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
 
 // ==============================|| DASHBOARD DEFAULT - POPULAR CARD ||============================== //
 
 const PopularCard = ({ isLoading }) => {
   const theme = useTheme();
-
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   const data = [
     { label: "Total", color: "secondary", percent: 15, value: 300 },
     { label: "Procesando", color: "info", percent: 10, value: 150 },
@@ -116,7 +99,8 @@ const PopularCard = ({ isLoading }) => {
                                       width: 16,
                                       height: 16,
                                       borderRadius: "5px",
-                                      backgroundColor: theme.palette[el.color].light,
+                                      backgroundColor:
+                                        theme.palette[el.color].light,
                                       color: theme.palette[el.color].dark,
                                       marginLeft: 1.875,
                                     }}
