ea56845dede1aa51ae3e98c8feebcf485b1710ad
Sandy Pérez Matamoros <sandypm007@gmail.com>
2023-07-11 19:49:54 -0300

    GitHub build (#4)
    * Adding github build
    
    * Adding github build
    
    * Adding build files
    
    * Using secrets
    
    * Build using main

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,5 +1,3 @@
-console.log(import.meta.env)
-
 export default {
     url: import.meta.env.VITE_WEB_URL ?? 'http://localhost:5173/',
     api_url: import.meta.env.VITE_BACKEND_URL ?? 'http://localhost:1130/',
